73cd133c027e9ebe3f4f3b0bf310dcf7d9a3f8d7
flavioespinoza <flavio.espinoza@gmail.com>
2021-06-19 00:46:43 -0600

    test - balanced

--- src/code/__tests__/balanced.node.js
+++ src/code/__tests__/balanced.node.js
@@ -2,10 +2,12 @@
 import balanced from '../balanced'
 
 test('valid parentheses', () => {
+  expect(balanced('[]]')).toEqual(false);
   expect(balanced('[()]{}{[()()]()}')).toEqual(true);
   expect(balanced('[(()]{}{[()()]()}')).toEqual(false);
   expect(balanced('[(])')).toEqual(false);
-  expect(balanced('[*()999]{}{[(22)(--)]()8}')).toEqual(true);
-  expect(balanced('[*(()999]{}{[(22)(--)]()8}')).toEqual(false);
-  expect(balanced('[*(]999)')).toEqual(false);
+  expect(balanced('[p]999]')).toEqual(false);
+  expect(balanced('[p()999]{}{[(22)(--)]()8}')).toEqual(true);
+  expect(balanced('[p(()999]{}{[(22)(--)]()8}')).toEqual(false);
+  expect(balanced('[p(]999)')).toEqual(false);
 });
