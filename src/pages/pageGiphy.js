00d5f61a035706c726c4310a0f9fb706fe3f6777
flavioespinoza <flavio.espinoza@gmail.com>
2021-07-29 17:56:02 -0600

    stuff

--- src/pages/pageGiphy.js
+++ src/pages/pageGiphy.js
@@ -1,6 +1,8 @@
 // @flow
-import React, {useContext, useState} from 'react';
-import {styled} from 'fusion-plugin-styletron-react';
+import React, { useContext, useState } from 'react';
+import { styled } from 'fusion-plugin-styletron-react';
+import { Box, Typography } from '@material-ui/core';
+
 import {
   Grid, // our UI Component to display the results
   SearchBar, // the search bar the user will type into
@@ -13,6 +15,26 @@ import Head from '../components/Head';
 const FullHeightDiv = styled('div', {
   height: '100%',
   backgroundColor: '#FFFFFF',
+});
+
+const Header = styled(Box, (props) => ({
+  width: '100%',
+  cursor: 'pointer',
+  padding: "0.5em 1em",
+  color: props.$isActive ? "#fff" : "#000",
+  background: props.$isActive ? "#276ef1" : "none",
+  fontSize: "1em",
+  borderRadius: "4px",
+  border: "1px solid #aaa",
+  ":hover": {
+    background: props.$isActive ? "green" : "yellow"
+  }
+}));
+
+const Content = styled('div', {
+  width: '100%',
+  overflow: 'hidden',
+  transition: 'all 0.5s ease',
 });
 
 const SearchDiv = styled('div', {
@@ -38,65 +60,80 @@ const ButtonIconClose = styled('button', {
 // define the components in a separate function so we can
 // use the context hook. You could also use the render props pattern
 const Components = () => {
-  const {setSearch, fetchGifs, searchKey} = useContext(SearchContext);
+  const { setSearch, fetchGifs, searchKey } = useContext(SearchContext);
+ 
   // eslint-disable-next-line cup/no-undef
   const [width, setWidth] = useState(window.innerWidth);
   const [columns, setColumns] = useState(1);
+
+  const [isOpened, setIsOpened] = React.useState(false);
+  const toggleOpen = React.useCallback(() => setIsOpened(!isOpened), [
+    isOpened,
+  ]);
+
   return (
-    <FullHeightDiv>
+    <div>
       <Head />
-      <SearchDiv>
-        <ButtonIconClose
-          onClick={() => setSearch('')}
-          style={{color: searchKey === '' ? 'white' : 'hotpink'}}
-        >
-          <i className="fad fa-times fa-xs"></i>
-        </ButtonIconClose>
-        <SearchBar autoFocus={true} clear={true} />
-        <ButtonIcon
-          onClick={() => setColumns(1)}
-          style={{color: columns === 1 ? 'hotpink' : ''}}
-        >
-          <i className="fad fa-th-list fa-lg"></i>
-        </ButtonIcon>
-        <ButtonIcon
-          onClick={() => setColumns(2)}
-          style={{color: columns === 2 ? 'hotpink' : ''}}
-        >
-          <i className="fad fa-th-large fa-lg"></i>
-        </ButtonIcon>
-        <ButtonIcon
-          onClick={() => setColumns(3)}
-          style={{color: columns === 3 ? 'hotpink' : ''}}
-        >
-          <i className="fad fa-th fa-lg"></i>
-        </ButtonIcon>
-      </SearchDiv>
-      <Grid
-        key={searchKey}
-        fetchGifs={fetchGifs}
-        noResultsMessage={
-          searchKey === '' ? '' : `No results found for: ${searchKey}`
-        }
-        width={width}
-        columns={columns}
-        gutter={6}
-      />
-      <ResizeObserver
-        onResize={({width}) => {
-          setWidth(width);
-        }}
-      />
-    </FullHeightDiv>
+      <Header $isActive={isOpened} onClick={toggleOpen}>
+        <Typography variant='h6' align='left'>
+          props.title
+        </Typography>
+        {isOpened ? (<Content>props.children</Content>) : null}
+      </Header>
+
+      <FullHeightDiv>
+        <SearchDiv>
+          <ButtonIconClose
+            onClick={() => setSearch('')}
+            style={{ color: searchKey === '' ? 'white' : 'hotpink' }}
+          >
+            <i className='fad fa-times fa-xs'></i>
+          </ButtonIconClose>
+          <SearchBar autoFocus={true} clear={true} />
+          <ButtonIcon
+            onClick={() => setColumns(1)}
+            style={{ color: columns === 1 ? 'hotpink' : '' }}
+          >
+            <i className='fad fa-th-list fa-lg'></i>
+          </ButtonIcon>
+          <ButtonIcon
+            onClick={() => setColumns(2)}
+            style={{ color: columns === 2 ? 'hotpink' : '' }}
+          >
+            <i className='fad fa-th-large fa-lg'></i>
+          </ButtonIcon>
+          <ButtonIcon
+            onClick={() => setColumns(3)}
+            style={{ color: columns === 3 ? 'hotpink' : '' }}
+          >
+            <i className='fad fa-th fa-lg'></i>
+          </ButtonIcon>
+        </SearchDiv>
+        <Grid
+          key={searchKey}
+          fetchGifs={fetchGifs}
+          noResultsMessage={
+            searchKey === '' ? '' : `No results found for: ${searchKey}`
+          }
+          width={width}
+          columns={columns}
+          gutter={6}
+        />
+        <ResizeObserver
+          onResize={({ width }) => {
+            setWidth(width);
+          }}
+        />
+      </FullHeightDiv>
+    </div>
   );
 };
 
 // the search experience consists of the manager and its child components that use SearchContext
-const PageGiphy = ({apiKey, rating, lang}) => {
+const PageGiphy = ({ apiKey, rating, lang }) => {
   return (
-    <SearchContextManager apiKey={apiKey} options={{rating, lang}}>
+    <SearchContextManager apiKey={apiKey} options={{ rating, lang }}>
       <Components />
-      {/* other components */}
     </SearchContextManager>
   );
 };
