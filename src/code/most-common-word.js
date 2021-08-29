a9dfec2ca13603e1a292663c8089bf5eace168ad
flavioespinoza <flavio.espinoza@gmail.com>
2021-08-29 12:42:41 -0600

    most common words

--- src/code/most-common-word.js
+++ src/code/most-common-word.js
@@ -1,9 +1,9 @@
-const paragraph = 'Bob hit a ball, the hit BALL flew far after it was hit.';
-const banned = ['hit'];
+// const paragraph = 'Bob hit a ball, the hit BALL flew far after it was hit.';
+// const banned = ['hit'];
 // Output: "ball"
 
-// const paragraph = 'a, a, a, a, b,b,b,c, c';
-// const banned = ['a'];
+const paragraph = 'a, a, a, a, b,b,b,c, c';
+const banned = ['a'];
 
 function mostCommonWord(paragraph, banned) {
   const remove = paragraph.split(/[\!\?\'\,\;\.\s]/);
@@ -20,7 +20,7 @@ function mostCommonWord(paragraph, banned) {
   const set = new Set([...excluded]);
   const map = {};
   for (let i = 0; i < words.length; i++) {
-    if (set.has(words[i].toLowerCase())) {
+    if (set.has(words[i])) {
       // do nothing
     } else if (map[words[i]]) {
       map[words[i]] = map[words[i]] + 1;
