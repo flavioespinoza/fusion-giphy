a82ec52e2e7f804f2a07cc56b7f11e7ba6441bb6
Flavio Espinoza <flavio.espinoza@gmail.com>
2021-10-04 17:25:06 -0600

    google fail

--- src/code/combign-fetchers.js
+++ src/code/combign-fetchers.js
@@ -1,53 +1,53 @@
-// Given a series of fetcher methods used for autocomplete, which accept a search string and a callback function. Write a method combineFetchers that can accept multiple fetchers and returns a new function which fetches a combination of results.
+// // Given a series of fetcher methods used for autocomplete, which accept a search string and a callback function. Write a method combineFetchers that can accept multiple fetchers and returns a new function which fetches a combination of results.
 
-/**
- * @example
- * type FetcherCallback = (results: string[]) => void
- * type Fetcher = (prefix: string, callback: FetcherCallback) => void
- * type FetcherComposer = (Fetcher[]) => Fetcher 
- * */
+// /**
+//  * @example
+//  * type FetcherCallback = (results: string[]) => void
+//  * type Fetcher = (prefix: string, callback: FetcherCallback) => void
+//  * type FetcherComposer = (Fetcher[]) => Fetcher 
+//  * */
 
-const callback = (res) => {
-  console.log(res)
-}
-const prefix = 'to';
+// const callback = (res) => {
+//   console.log(res)
+// }
+// const prefix = 'to';
 
-fetchUsers((results) => {
-    console.log(results); // [‘Tom Jones’, ‘Tom Peters’]
-});
+// fetchUsers((results) => {
+//     console.log(results); // [‘Tom Jones’, ‘Tom Peters’]
+// });
 
-fetchArticles((result) => {
-  console.log(result)
-})
+// fetchArticles((result) => {
+//   console.log(result)
+// })
 
-const x = fetchUsers(callback)
+// const x = fetchUsers(callback)
 
-const fetchArticles = (prefix, callback);
+// const fetchArticles = (prefix, callback);
 
-const y = fetchArticles(prefix, callback)
+// const y = fetchArticles(prefix, callback)
 
-const getStuff = combineFetchers([fetchUsers, fetchArticles])
-
-
-getStuff('to', (results) => {
-  console.log(results); // [‘To be or not to be’, ‘Tom Jones’, ‘Tom Peters’]    
-});
-
-function combineFetchersFail(array) {
-  const result = [];
-  const get = async (prefix, callback) => {
-     for (const fetcher of array) {
-       const temp = await fetcher(prefix, callback);
-       result.push(...temp);
-     }
-    return result;
-  }
-  return get;
-}
+// const getStuff = combineFetchers([fetchUsers, fetchArticles])
 
 
-function combineFetchers(array) {
-  const fetchOne = array[2];
-  const fetchTwo = array[1];
+// getStuff('to', (results) => {
+//   console.log(results); // [‘To be or not to be’, ‘Tom Jones’, ‘Tom Peters’]    
+// });
+
+// function combineFetchersFail(array) {
+//   const result = [];
+//   const get = async (prefix, callback) => {
+//      for (const fetcher of array) {
+//        const temp = await fetcher(prefix, callback);
+//        result.push(...temp);
+//      }
+//     return result;
+//   }
+//   return get;
+// }
+
+
+// function combineFetchers(array) {
+//   const fetchOne = array[2];
+//   const fetchTwo = array[1];
   
-}
+// }
--- src/code/combine-fetchers.ts
+++ src/code/combine-fetchers.ts
@@ -1,41 +1,54 @@
-let items = [1, 2];
+/**
+ * @example
+ * type FetcherCallback = (results: string[]) => void
+ * type Fetcher = (prefix: string, callback: FetcherCallback) => void
+ * type FetcherComposer = (Fetcher[]) => Fetcher 
+ * */
 
-function callMeMaybe(callback: () => void) {
-  callback();
+function myCallback(prefix: string, callback: (prefix: string) => void) {
+  return callback(prefix);
 }
 
-const getUser: (n: number) => void = (n: number) => {
-  return Math.random() > n ? ['Tom Balls'] : ['Tom Tits'];
-};
-
-// OR this:
-const getArticles: () => void = () => {
-  return ['Tits on the Top.', 'Tits on the bottom'];
-};
-
-type FetcherCallback = (results: string[]) => void
- 
-function _getArticles(prefix: string, callback: FetcherCallback) {
-  return callback;
+// this function returns a number
+function aNumberCallback(): number {
+  return 2;
 }
 
-// Expected an error because 'doSomething' returns number, but 'callMeMaybe'
-// expects void-returning function
-async function getFetchers(n: number) {
-  let result = [];
-  try {
-    const user = await getUser(n);
-    const articles = await getArticles();
-    return [user, articles];
-  } catch (err) {
-    throw err;
-  }
+// this function returns a number
+function fetchUsersCallBack(prefix: string): string[] {
+  return ['Tom Titties', 'Cheech Cheechees', 'Becky Balls', prefix];
+}
+function fetchArticlesCallback(prefix: string): string[] {
+  return ['Titties on Top', 'Cheechees and Change', 'Becky Blows Boston'];
+}
+
+// works 👍 type safety is ensured in doSomething
+async function fetchers(array: any) {
+  const arr = [];
+  const get = async (prefix: string, callback) => {
+    for (let i = 0; i < array.length; i++) {
+      const fetch = array[i];
+      arr.push(...callback(prefix, fetch));
+    }
+    return arr;
+  };
+  return get;
 }
 
 (async function() {
   try {
-    const res = await getFetchers(666);
-    console.log(res); // [ [ 'Tom Tits' ], [ 'Tits on the Top.', 'Tits on the bottom' ] ]
+    const getStuff = await fetchers([fetchUsersCallBack, fetchArticlesCallback]);
+    const res = await getStuff('to', myCallback);
+    console.log(res);
+    const output = [
+      'Tom Titties',
+      'Cheech Cheechees',
+      'Becky Balls',
+      'to',
+      'Titties on Top',
+      'Cheechees and Change',
+      'Becky Blows Boston',
+    ];
   } catch (err) {
     throw err;
   }
