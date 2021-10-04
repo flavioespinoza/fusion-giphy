22e6e85609d53b772eb33cf5c63999b4893d21b7
Flavio Espinoza <flavio.espinoza@gmail.com>
2021-10-04 00:25:47 -0600

    word ladder Breadth First Search

--- src/code/word-ladder.js
+++ src/code/word-ladder.js
@@ -4,53 +4,57 @@
  * @param {string[]} wordList
  * @return {number}
  */
-const ladderLength = function (beginWord, endWord, wordList) {
-    let len = 1;
-    let queue = [beginWord];
-    const available = Array.from(new Set([...wordList.join('')]));
-    console.log(available)
-    const dict = new Set(wordList);
-    console.log(dict)
-    const seen = new Set(queue);
-    console.log(seen)
-    while (queue.length) {
-      const next = [];
-      
-      for (let v of queue) {
-        if (v === endWord) {
-          return len;
-        }
-        
-        const arr = v.split('');
-        console.log(arr)
-        // ['h', 'o', 't']
-        
-        for (let i = 0; i < arr.length; i++) {
-          for (let d = 0; d < available.length; d++) {
-            arr[i] = available[d]; // 126 combinations vs 468 if you use the entire alphabet
-        
-            const nv = arr.join('');
+const ladderLength = function(beginWord, endWord, wordList) {
+  let len = 1;
+  let queue = [beginWord];
+  const available = Array.from(new Set([...wordList.join('')]));
+  console.log(available); // [ 'h', 'o', 't', 'd', 'g', 'l', 'c' ]
+  const dict = new Set(wordList);
+  console.log(dict); // Set { 'hot', 'dot', 'dog', 'lot', 'log', 'cog' }
+  const seen = new Set(queue);
+  console.log(seen); // Set { hit }
+  while (queue.length) {
+    const next = [];
 
-            console.log(nv)
+    for (let word of queue) {
+      if (word === endWord) {
+        return len;
+      }
 
-            
-            if (!seen.has(nv) && dict.has(nv)) {
-              next.push(nv);
-              seen.add(nv);
-            }
-            arr[i] = v[i];
+      const arr = word.split('');
+      console.log(arr);
+      // ['h', 'o', 't']
+
+      for (let i = 0; i < arr.length; i++) {
+        for (let d = 0; d < available.length; d++) {
+          arr[i] = available[d]; // 126 combinations vs 468 if you use the entire alphabet
+          
+          const nv = arr.join('');
+          console.log(nv); // hit, oit, tit, dit
+
+          if (!seen.has(nv) && dict.has(nv)) {
+            next.push(nv);
+            seen.add(nv);
           }
+          arr[i] = word[i];
         }
       }
-      queue = next;
-      len++;
     }
-    return 0;
-  };
+    console.log(seen); 
+    // Set { 'hit', 'hot' }
+    // Set { 'hit', 'hot', 'dot' }
+    // Set { 'hit', 'hot', 'dot', 'lot' }
+    // Set { 'hit', 'hot', 'dot', 'lot', 'dog', 'log' }
+    // Set { 'hit', 'hot', 'dot', 'lot', 'dog', 'log', 'cog' }
 
+    queue = next;
+    len++;
+  }
+  return 0;
+};
 
-  let beginWord = "hit"
-  let endWord = "cog"
-  let wordList = ["hot","dot","dog","lot","log","cog"]
+let beginWord = 'hit';
+let endWord = 'cog';
+let wordList = ['hot', 'dot', 'dog', 'lot', 'log', 'cog'];
 
-  console.log(ladderLength(beginWord, endWord, wordList));
+console.log(ladderLength(beginWord, endWord, wordList));
