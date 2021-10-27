1e9d2b39ea8100a919f9de6fa82005be544b00c3
Flavio Espinoza <flavio.espinoza@gmail.com>
2021-10-27 08:17:43 -0600

    words differ by k

--- src/code-google/bfs-iterative__shortest-path-in-unidirectional-graph.js
+++ src/code-google/bfs-iterative__shortest-path-in-unidirectional-graph.js
@@ -28,34 +28,6 @@ let edges = [
   ['w', 'v'],
 ];
 
-console.log(shortestPath(edges, 'w', 'z')); // 2
+// console.log(shortestPath(edges, 'w', 'z')); // 2
 
-/**
- * @param {word1} string current word
- * @param {word2} string word to compare
- * @param {k} number max number of changes allowed
- * */
-function wordsDifferByK(word1, word2, k) {
-  if (word1 === word2) throw new Error('Error: word1 and word2 must be different words');
-  if (word1.length === word2.length) {
-    // if word1 is as long as word 2
-    for (let i = 0; i < word1.length; i++) {
-      // go from first to last character index the words
-      if (word1.charAt(i) != word2.charAt(i)) {
-        // if this character from word_1 does not equal the character from word_2
-        // decrement changes allowed
-        k--; 
-        if (k < 0) {
-          // and if you have more changes than allowed
-          return false; // return false
-        }
-      }
-    }
-  }
-
-  return true;
-}
-
-console.log(wordsDifferByK('hit', 'hot', 1)); // true
-console.log(wordsDifferByK('hit', 'cot', 1)); // false
-// console.log(wordsDifferByK('hit', 'hit', 1)); // Error: word1 and word2 must be different words
+module.exports = shortestPath;
