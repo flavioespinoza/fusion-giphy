9e72f58f338bb5a6db173d69d33c9436b8019204
Flavio Espinoza <flavio.espinoza@gmail.com>
2021-10-26 21:59:20 -0600

    shortest-path-in-binary-matrix

---
+++ src/code/shortest-path-in-a-binary-matrix.js
@@ -0,0 +1,84 @@
+/**
+ * @name shortest-distance-in-binary-matrix
+ * @solution BFS (Breadth First Search); shortest path reminds me of word-ladder, but way less complicated.
+ * @constraint can only move [up, down, left, right]
+ * @url https://leetcode.com/problems/shortest-path-in-binary-matrix/discuss/313252/javascript-solution-bfs
+ * @example 
+ * This is actually the question of finding shortest path between source and destination in a 2D-array.
+ * Instead of moving on 4 directions, we need to move to 8 directions for this question.
+ * 
+ * C_i and C_i + 1 are different and share an edge or corner
+ * 
+ * We need keep tracking cells that have been visited. 
+ * Usually we can create a same size 2D-array to mark visited cell, 
+ * for this question, however, it did not mention that we can not modify the original 
+ * 2D-array, so we can just flip 0 to 1 to mark it as visited
+ * 
+ * */
+function shortestPath(array) {
+  
+  if (array[0][0]) return -1;
+  
+  const que = [{ coord: [0, 0], dist: 1 }];
+
+  // @note : all directions if there was no constraint [ left, right, up, down, left-up, up-right, down-right, down-left ]
+  // const directions = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+  // @note : only 4 directions because of cardinal constraints
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  // height and width of matrix
+  const H = array.length;
+
+  // check if coord is valid
+  const isValidCoord = (x, y) => x >= 0 && x < H && y >= 0 && y < H;
+
+  // start coord is always zero so we know it will be visited
+  // flip 0 to 1 to keep track of visited
+  array[0][0] = -1;
+
+  // while que has object(s)
+  while (que.length) {
+    // destructure first object on the que to get vars x, y, and dist
+    const {
+      coord: [x, y],
+      dist,
+    } = que.shift();
+    // if reached bottom-right return dist
+    if (x === H - 1 && y === H - 1) {
+      return dist;
+    }
+    // for of const [moveX, moveY] of direction
+    for (const [moveX, moveY] of directions) {
+      // nextX and nextY
+      const nextX = moveX + x;
+      const nextY = moveY + y;
+      // check coord is valid and value at coord is 0;  
+      if (isValidCoord(nextX, nextY) && array[nextX][nextY] === 0) {
+        // push({ coord: [nextX, nextY], dist++})
+        que.push({ coord: [nextX, nextY], dist: dist + 1 });
+          // flip 0 to 1 to keep track of visited
+        array[nextX][nextY] = 1;
+      }
+    }
+  }
+  return -1;
+}
+
+
+
+const input = [
+  [0,1,0,0,0],
+  [0,1,0,1,0],
+  [0,1,0,1,0],
+  [0,1,0,1,0],
+  [0,0,0,1,0]
+]
+
+const expected = 17;
+
+console.log(shortestPath(input)); // 17
--- src/code/sort-array-linear.js
+++ src/code/sort-array-linear.js
@@ -14,7 +14,7 @@ function sortLinear(array) {
   // loop 2
   for (const [key, val] of Object.entries(map)) {
     if (val > 1) {
-      sorted = [...sorted, ...new Array(val).fill(key)]
+      sorted = [...sorted, ...new Array(val).fill(key)];
     } else {
       sorted.push(key);
     }
@@ -25,5 +25,141 @@ function sortLinear(array) {
   };
 }
 
-
 console.log(sortLinear([5, 4, 3, 2, 1, 0, 0, 0]));
+
+// Given an n x n matrix of 1s and 0s, 
+
+// return the length of the shortest path of 0's from the top left cell to the bottom right cell. 
+
+// You may only move up/down/left/right. 
+
+// If there is no possible path, return -1.
+
+// Examples:
+
+// Input:
+// [[0,0,1],
+//  [1,0,1],
+//  [1,0,0]]
+
+// 0, 0, 1
+// 1, 0, 1
+// 1, 0, 0
+
+// Output: 5 
+//.       x,y
+// Path: [0,0] -> [0,1] -> [1,1] -> [2,1] -> [2,2]
+
+// Input:
+// [[0,0,0],
+//  [0,1,0],
+//  [0,0,0]]
+// Output: 5 
+
+// Input:
+// [[0,0,0,1,1],
+//  [1,1,0,1,1],
+//  [1,1,0,1,1],
+//  [1,1,0,1,1],
+//  [1,1,0,1,0]]
+// Output: -1
+
+// Input:
+// [[0,1,0,0,0],
+//  [0,1,0,1,0],
+//  [0,1,0,1,0],
+//  [0,1,0,1,0],
+//  [0,0,0,1,0]]
+// Output: 17
+
+// Input:
+// [[0,0,0,0,0],
+//  [0,1,0,1,0],
+//  [0,1,0,1,0],
+//  [0,1,0,1,0],
+//  [0,0,0,1,0]]
+// Output: 9
+
+// Input:
+// [[1,1,1,1,1],
+//  [0,0,0,0,1],
+//  [0,0,0,0,1],
+//  [0,0,0,0,1],
+//  [0,0,0,0,1]]
+//  Output: 9
+
+// == strategy ==
+// x
+// y
+// p1 [0, 0]
+// p2 [0, 1] === 0
+// if true move pointers
+// while (condition) 
+// else 
+// p2 [1, 0] === 0
+// if true move pointers
+// else return -1
+
+
+
+function shortestPath(array) {
+  
+  if (array[0][0]) return -1;
+  
+  // que =[{ coord [0,0], dist: 1}]
+  // directions = [[-1, -1] [-1, 0]...]
+  // H = array.length
+  // isValidCoor = (x, y) => valid or not
+
+  const que = [{ coord: [0, 0], dist: 1 }];
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+  const H = array.length;
+  const isValidCoord = (x, y) => x >= 0 && x < H && y >= 0 && y < H;
+
+  array[0][0] = -1;
+
+  while (que.length) {
+    const {
+      coord: [x, y],
+      dist,
+    } = que.shift();
+    
+    if (x === H - 1 && y === H - 1) {
+      return dist;
+    }
+
+    // for of const [moveX, moveY] of direction
+    for (const [moveX, moveY] of directions) {
+      // nextX and nextY
+      const nextX = moveX + x;
+      const nextY = moveY + y;
+      if (isValidCoord(nextX, nextY) && array[nextX][nextY] === 0) {
+        // push({ coord: [nextX, nextY], dist++})
+        que.push({ coord: [nextX, nextY], dist: dist + 1 });
+        array[nextX][nextY] = 1;
+      }
+    }
+
+    
+  }
+  return -1;
+}
+
+
+
+const input = [
+  [0,1,0,0,0],
+  [0,1,0,1,0],
+  [0,1,0,1,0],
+  [0,1,0,1,0],
+  [0,0,0,1,0]
+]
+
+const expected = 17;
+
+console.log(shortestPath(input)); // 17
