e2003e4029268ee3e069a4238d87a741a5dba717
Flavio Espinoza <flavio.espinoza@gmail.com>
2021-10-26 19:20:04 -0600

    k-smallest-largest-unsorted-array

--- src/code/min-order-statistics.js
+++ src/code/min-order-statistics.js
@@ -23,19 +23,20 @@ function kSmallest(array, k) {
   let frequencies = 0;
   // loop 2
   for (const [key, val] of Object.entries(map)) {;
+    // @note : the sorted array is not necessary for this solution
+    // @note : the sorted array helps demonstrate how this solution works
+    // @note : the sorted array shows how it could be used in a linear sort if the k-th value was not required
+    // @note : see ./src/code/sort-array-linear.js
     sorted.push(key);
     frequencies = frequencies + val;
     // when the value of frequencies is greater-than-or-equal-to 
     // the value of k return the key
     if (frequencies >= k) {
-      console.log(sorted); // [ '0', '4', '7' ] [ '3', '5', '7', '10' ] [ '1', '5' ] [ '0', '6' ]
       const last = sorted[sorted.length - 1];
       if (key === last) {
-        /**
-         * @example
-         * Notice how the last element of sorted equals the key of the map.
-         * Basically the iteration stops when the k-th value has been pushed onto the sorted array
-         * */
+        // @note : the iteration stops when the k-th value has been pushed onto the sorted array
+        console.log(sorted); // [ '0', '4', '7' ] [ '3', '5', '7', '10' ] [ '1', '5' ] [ '0', '6' ]
+        // @note : note how the last element of sorted equals the key of the map
         console.log(key);   // 7, 10, 5, 6
         console.log(last);  // 7, 10, 5, 6
       }
