3d78a0f1b11febfec76895a387ae686bd906fe6f
Flavio Espinoza <flavio.espinoza@gmail.com>
2022-01-21 15:21:43 -0700

    curry

--- algos/currying/curry.js
+++ algos/currying/curry.js
@@ -5,11 +5,11 @@ const curry = (fn) => {
       return curried.bind(null, ...args);
     }
     return fn(...args);
-  }
-}
+  };
+};
 
 const total = (x, y, z) => x + y + z;
 
 const curriedTotal = curry(total);
 
-console.log(curriedTotal(10)(20)(30));
+console.log(curriedTotal(10)(20)(30)); // 60
