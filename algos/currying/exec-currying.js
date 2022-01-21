d860470245ae8666eeaa16ebf809c69d371ce961
Flavio Espinoza <flavio.espinoza@gmail.com>
2022-01-21 14:27:43 -0700

    curry

--- algos/currying/exec-currying.js
+++ algos/currying/exec-currying.js
@@ -2,7 +2,10 @@ function increment(n) {
   return n + 1;
 }
 
+const increment2 = (n) => n + 1;
+
 console.log(increment(3)); // 4
+console.log(increment2(3)); // 4
 
 function add(a) {
   return function (b) {
@@ -10,7 +13,10 @@ function add(a) {
   }
 }
 
+const add2 = (a) => (b) => a + b;
+
 console.log(add(2)(4)); // 6
+console.log(add2(2)(4)); // 6
 
 function divide(numerator) {
   return function (denominator) {
@@ -18,9 +24,22 @@ function divide(numerator) {
   }
 }
 
+const divide2 = (numerator) => (denominator) => numerator / denominator;
+
 console.log(divide(8)(4)); // 2
+console.log(divide2(8)(4)); // 2
 
-
-function exec(initialValue, ...args) {
-  const res = args.reduce()
+function exec(...args) {
+  return function(n) {
+    let current = n;
+    for (const fn of args) {
+      current = fn(current);
+    }
+    return current;
+  }
 }
+
+const exec2 = (...args) => (n) => args.reduce((acc, fn) => fn(acc), n);
+
+console.log(exec(divide(8), add(3), increment)(4)); // 6
+console.log(exec2(divide(8), add(3), increment)(4)); // 6
